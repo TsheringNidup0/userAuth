54cdc715687e010123e69f026e24c0ac441e4df4
TsheringNidup0 <niduptshering036@gmail.com>
2025-06-26 11:54:05 +0600

    jjj

--- config/db.js
+++ config/db.js
@@ -1,19 +1,15 @@
-const pgpInit = require('pg-promise')
+const pgp = require('pg-promise')();
 require('dotenv').config();
 
-
-const pgp = pgpInit();
-
-
+const isProduction = process.env.NODE_ENV === 'production';
 
 const db = pgp({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
+  password: process.env.DB_PASSWORD,
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 module.exports = db;
-
